9727f9f177f7e1b5d26b104e6f478d4b2fb78507
karandarjishack <102816448+karandarjishack@users.noreply.github.com>
2024-02-12 19:15:33 -0500

    tools.sh

--- tools.sh
+++ tools.sh
@@ -1,15 +1,25 @@
 #Before running the script, make sure to make the following things
 #You are in root
-echo "Let the Fun Begin"
 
+#This script automates the setup process for various security and web testing tools on a Linux system.
+
+echo "Let the Fun Begin"
+#Updates the package lists and upgrades existing packages using apt.
 sudo apt update
 sudo apt upgrade
+
+#Downloads and extracts the Go programming language version 1.18.2.
 wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz
+
 gunzip go1.18.2.linux-amd64.tar.gz
 tar -xvf go1.18.2.linux-amd64.tar
+#Sets environment variables for Go modules.
 cp go/bin/go /usr/local/bin
 export GO111MODULE=on
 cp -R go /usr/local
+
+#Installs subfinder, nuclei, httpx, dalfox, gf, waybackurls, interactsh, qsreplace, and ffuf using Go's package manager.
+
 wget https://github.com/projectdiscovery/subfinder/releases/download/v2.4.8/subfinder_2.4.8_linux_amd64.tar.gz
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 git clone https://github.com/projectdiscovery/nuclei-templates.git
@@ -26,7 +36,9 @@ git clone https://github.com/iamj0ker/bypass-403.git
 git clone https://github.com/danielmiessler/SecLists.git
 go install github.com/ffuf/ffuf@latest
 
+#Clones various GitHub repositories for additional tooling and wordlists.
 
+#Extracts compressed files and removes unnecessary archives.
 gunzip *.gz
 tar -xvf subfinder_2.4.8_linux_amd64.tar
 tar -xvf httpx_1.1.0_linux_amd64.tar
@@ -34,8 +46,10 @@ tar -xvf dalfox_2.7.5_linux_amd64.tar
 
 unzip *.zip
 
+#Cleans up by removing temporary files.
 rm -rf *.tar
 
+#Moves executables to /usr/local/bin for global accessibility.
 
 mv subfinder /usr/local/bin
 mv httpx /usr/local/bin
@@ -49,5 +63,7 @@ mv interactsh-client /usr/local/bin
 mv qsreplace /usr/local/bin
 mv ffuf /usr/local/bin
 cd ../../
+
+#Prints "Done" to indicate the completion of the setup process.
 
 echo "Done"
